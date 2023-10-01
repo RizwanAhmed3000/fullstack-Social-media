da3efbfda7fb86e4bc43fc0243786a395b648b7c
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-10-01 15:23:08 +0500

    login form

--- frontend-react/src/pages/login/Login.jsx
+++ frontend-react/src/pages/login/Login.jsx
@@ -1,6 +1,18 @@
+import { useRef } from "react";
 import "./login.css"
 
 export default function Login() {
+
+    const email = useRef();
+    const password = useRef();
+
+    function loginHandler(e) {
+        e.preventDefault()
+        console.log("loginHandler working");
+        console.log(email.current.value);
+        // console.log(password.current.value);
+    }
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -9,19 +21,19 @@ export default function Login() {
                     <p className="tagline">This is the tag line of SocialMedia</p>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
+                    <form className="loginBox" onSubmit={loginHandler}>
                         <div class="form__group field">
-                            <input type="email" class="form__field" placeholder="Email" required="" />
-                                <label for="name" class="form__label">Email Address</label>
+                            <input type="email" class="form__field" placeholder="Email" required ref={email} />
+                            <label for="name" class="form__label">Email Address</label>
                         </div>
                         <div class="form__group field">
-                            <input type="password" class="form__field" placeholder="Email" required="" />
+                            <input type="password" class="form__field" placeholder="Email" required minLength={8} ref={password} />
                             <label for="name" class="form__label">Password</label>
                         </div>
-                        <button className="loginBtn seeMoreBtn">Log in</button>
+                        <button className="loginBtn seeMoreBtn" >Log in</button>
                         <span className="forgetPassword">Forget password</span>
                         <button className="signupBtn seeMoreBtn">Create new account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
