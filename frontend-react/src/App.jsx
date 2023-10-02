834a0c1cd64422ed23a8bbdd89fa61438882a9c8
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-10-02 12:15:53 +0500

    redirecting pages

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,16 +2,19 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profilePage/Profile";
 import Signup from "./pages/signup/Signup";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes, redirect } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const {user} = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/profile/:username" element={<Profile />} />
+        <Route exact path="/" element={user ? <Home /> : <Login/>} />
+        <Route path="/login" element={user ? redirect('/') : <Login/>} />
+        <Route path="/signup" element={user ? redirect('/') : <Signup />} />
+        <Route  path="/profile/:username" element={<Profile />} />
       </Routes>
     </Router>
   );
